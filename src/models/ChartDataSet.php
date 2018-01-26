4b419e09fc9554eeb9898619a32e8b1e7f920175
Louis Dekeister <louis@iMac-de-Louis.local>
2018-01-25 19:08:34 -0800

    add attributes to ChartDataSet model

--- src/models/ChartDataSet.php
+++ src/models/ChartDataSet.php
@@ -32,15 +32,33 @@ class ChartDataSet extends Model
     // Public Properties
     // =========================================================================
 
-    /**
-     * Some model attribute
-     *
-     * @var string
-     */
-    public $someAttribute = 'Some Default';
+    public $label;
+    public $data;
+    // public $backgroundColor;
+    public $borderColor;
+    public $fill;
+
+    public $lineColors = array(
+        'rgb(54, 162, 235)', //'blue'
+        'rgb(75, 192, 192)', //'green'
+        'rgb(201, 203, 207)', //'grey'
+        'rgb(255, 159, 64)', //'orange'
+        'rgb(153, 102, 255)', //'purple'
+        'rgb(255, 99, 132)', //'red'
+        'rgb(255, 205, 86)', //'yellow'
+    );
 
     // Public Methods
     // =========================================================================
+
+    public function __construct()
+    {
+        $this->label = '';
+        $this->data = array();
+        $this->fill = FALSE;
+        // $this->backgroundColor = $this->lineColors[rand(0, (count($this->lineColors) - 1))];
+        $this->borderColor = $this->lineColors[rand(0, (count($this->lineColors) - 1))];
+    }
 
     /**
      * Returns the validation rules for attributes.
@@ -55,8 +73,11 @@ class ChartDataSet extends Model
     public function rules()
     {
         return [
-            ['someAttribute', 'string'],
-            ['someAttribute', 'default', 'value' => 'Some Default'],
+            // ['someAttribute', 'string'],
+            // ['someAttribute', 'default', 'value' => 'Some Default'],
+            ['label', 'string'],
+            ['fill', 'boolean'],
+            ['borderColor', 'string']
         ];
     }
 }
