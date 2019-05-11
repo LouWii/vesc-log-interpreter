8316933dba5c0e7562a2d7b04aacf4fa11809468
Louis <link14.online@gmail.com>
2019-05-11 16:56:42 -0700

    add new model in prevision for refactor

---
+++ src/models/DataType.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * VESC Log Interpreter plugin for Craft CMS 3.x
+ *
+ * Process log data from VESC Monitor to generate charts
+ *
+ * @link      http://github.com/louwii
+ * @copyright Copyright (c) 2018 Louwii
+ */
+
+namespace louwii\vescloginterpreter\models;
+
+use craft\base\Model;
+
+/**
+ * DataType Model
+ * 
+ * A data type stores all the values for a specific type of data (MotorCurrent or Speed or Distance...)
+ *
+ * @author    Louwii
+ * @package   VescLogInterpreter
+ * @since     1.0.0
+ */
+class DataType extends Model
+{
+    private $name;
+
+    private $values;
+
+    public function __construct()
+    {
+        $this->values = array();
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function addValue($value)
+    {
+        $this->values[] = $value;
+    }
+
+    /**
+     * @return array
+     */
+    public function getValues()
+    {
+        return $this->values;
+    }
+}
